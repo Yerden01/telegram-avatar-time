87eecf54c09b201f21324590aaee483a725bbb1d
Yerden01 <mr.bulanbaev@gmail.com>
2022-10-03 09:02:59 +0600

    added multi user logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from telethon import TelegramClient
 from telethon.tl.functions.photos import UploadProfilePhotoRequest, DeletePhotosRequest
-from config import api_hash, api_id
+from config import users
 from utils import time_has_changed, generate_time_image_bytes
 from datetime import datetime, timedelta
 import argparse
@@ -16,15 +16,21 @@ def valid_tz(s):
         raise argparse.ArgumentTypeError(msg)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--api_id", required=False, help="user api ID", type=str, default=api_id)
-parser.add_argument("--api_hash", required=False, help="user api Hash", type=str, default=api_hash)
-parser.add_argument("--tz", required=False,  help="user api Hash", type=valid_tz, default=valid_tz('Asia/Almaty'))
+clients = []
 
-args = parser.parse_args()
+for user in users:
+    print("Starting parsing user: ", user[0])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--api_id", required=False, help="user api ID", type=str, default=user[1])
+    parser.add_argument("--api_hash", required=False, help="user api Hash", type=str, default=user[2])
+    parser.add_argument("--tz", required=False, help="user api Hash", type=valid_tz, default=valid_tz('Asia/Almaty'))
 
-client = TelegramClient("carpediem", args.api_id, args.api_hash)
-client.start()
+    args = parser.parse_args()
+
+    createdClient = TelegramClient(user[0], args.api_id, args.api_hash)
+    createdClient.start()
+
+    clients.append(createdClient)
 
 
 async def main():
@@ -33,12 +39,17 @@ async def main():
     while True:
         if time_has_changed(prev_update_time):
             bts = generate_time_image_bytes(datetime.now(args.tz).replace(tzinfo=None))
-            await client(DeletePhotosRequest(await client.get_profile_photos('me')))
-            file = await client.upload_file(bts)
-            await client(UploadProfilePhotoRequest(file))
+
+            for client in clients:
+                await client(DeletePhotosRequest(await client.get_profile_photos('me')))
+                file = await client.upload_file(bts)
+                await client(UploadProfilePhotoRequest(file))
+
             prev_update_time = datetime.now()
             time.sleep(1)
 
+
 if __name__ == '__main__':
     import asyncio
+
     asyncio.get_event_loop().run_until_complete(main())
