e7ee15fa0db11ac119dc5ea098ec8ad28aba5fca
Yerden01 <mr.bulanbaev@gmail.com>
2022-10-06 02:25:29 +0600

    added moviepy

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ apt update -y
 apt dist-upgrade -y
 apt install -y python3 python3-pip python3-pkgconfig python3-cairo python3-testresources libsm6 libxext6 libxrender-dev libcurl4-openssl-dev libssl-dev ffmpeg libsm6 libxext6 -y
 sudo python3 -m pip install -U pip
-pip3 install -U telethon numpy opencv-python pillow pytz --user --no-cache-dir
+pip3 install -U telethon numpy opencv-python pillow pytz moviepy --user --no-cache-dir
 until python3 main.py; do
     sleep 1
 done
